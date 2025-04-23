36027101a4efcf0094788252e8e56b5af92cfd9d
Your Name <you@example.com>
2025-04-23 18:42:04 +0200

    rr mié 23 abr 2025 18:42:04 CEST

--- zz/p12_fastapi_pdfs_2_react_public.py
+++ zz/p12_fastapi_pdfs_2_react_public.py
@@ -6,27 +6,28 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 origen = '/home/pk/Desktop/backend/app/routers'
 destino = '../public'
-
-# Ruta completa de destino
 destino_completo = os.path.join(destino, 'routers')
+
+# Función para ignorar archivos o carpetas con '.~lock' o '__' en el nombre
+def ignore_special_files(dir, files):
+    return [f for f in files if '.~lock' in f or '__' in f]
 
 # Eliminar la carpeta de destino si existe
 if os.path.exists(destino_completo):
     shutil.rmtree(destino_completo)
 
-# Copiar recursivamente la carpeta de origen al destino
-shutil.copytree(origen, destino_completo)
+# Copiar la carpeta, ignorando los archivos/carpetas que cumplan la condición
+shutil.copytree(origen, destino_completo, ignore=ignore_special_files)
 
-for root, dirs, files in os.walk('../public/routers'):
+# Eliminar archivos .py y la carpeta assets
+for root, dirs, files in os.walk(destino_completo):
     for file in files:
         if file.endswith('.py'):
             ruta_completa = os.path.join(root, file)
             print(f'Eliminando: {ruta_completa}')
             os.remove(ruta_completa)
 
-
-    # Eliminar la carpeta 'assets' si existe
     if 'assets' in dirs:
         ruta_assets = os.path.join(root, 'assets')
         print(f'Eliminando carpeta assets: {ruta_assets}')
-        shutil.rmtree(ruta_assets)  # Elimina la carpeta 'assets' y su contenido
+        shutil.rmtree(ruta_assets)
